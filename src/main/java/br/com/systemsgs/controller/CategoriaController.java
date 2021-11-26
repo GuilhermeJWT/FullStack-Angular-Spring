8791f83a57e89aa602f113124b887bff93a26ca2
GuilhermeJWT <guiromanno@gmail.com>
2021-11-26 15:16:09 -0300

    Criando mais alguns endpoints

--- src/main/java/br/com/systemsgs/controller/CategoriaController.java
+++ src/main/java/br/com/systemsgs/controller/CategoriaController.java
@@ -1,11 +1,20 @@
 package br.com.systemsgs.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.systemsgs.model.ModelCategoria;
 import br.com.systemsgs.repository.CategoriaRepository;
@@ -20,6 +29,21 @@ public class CategoriaController {
 	@GetMapping(value = "/listar")
 	public List<ModelCategoria> listar(){
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping(value = "/salvar")
+	public ResponseEntity<ModelCategoria> salvar(@RequestBody ModelCategoria modelCategoria, HttpServletResponse response) {
+		ModelCategoria categoriaSalva = categoriaRepository.save(modelCategoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping(value = "/{codigo}")
+	public Optional<ModelCategoria> pesquisaPorId(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo);
 	}
 
 }
