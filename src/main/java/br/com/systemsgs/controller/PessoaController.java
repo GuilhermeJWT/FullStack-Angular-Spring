a72af4faa97b2d782ec91918d37a89ff86e07d7d
GuilhermeJWT <guiromanno@gmail.com>
2021-11-27 22:22:17 -0300

    Implementando o endpoint de delete e tratando exceção

--- src/main/java/br/com/systemsgs/controller/PessoaController.java
+++ src/main/java/br/com/systemsgs/controller/PessoaController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.systemsgs.event.RecursoCriadoEvent;
@@ -48,6 +50,12 @@ public class PessoaController {
 	@GetMapping(value = "/{codigo}")
 	 public ModelPessoa recuperaPorCodigo(@PathVariable Long codigo) {
 		return pessoaRepository.findById(codigo).orElseThrow(() -> new RecursoNaoEncontradoException());
+	}
+	
+	@DeleteMapping(value = "/delete/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		 pessoaRepository.deleteById(codigo);
 	}
 	
 }
