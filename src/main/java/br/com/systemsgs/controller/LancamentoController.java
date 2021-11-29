e1ae9fc5cf113d504087f15fdd9bdd424e0fa374
GuilhermeJWT <guiromanno@gmail.com>
2021-11-29 17:42:10 -0300

    Salvando um Lançamento

--- src/main/java/br/com/systemsgs/controller/LancamentoController.java
+++ src/main/java/br/com/systemsgs/controller/LancamentoController.java
@@ -2,22 +2,35 @@ package br.com.systemsgs.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.systemsgs.model.ModelLancamentos;
-import br.com.systemsgs.repository.LancamentoRepository;
 import br.com.systemsgs.service.LancamentosService;
 
-@RestController()
+@RestController
 @RequestMapping(value = "/api/lancamentos")
 public class LancamentoController {
 
 	@Autowired
 	private LancamentosService lancamentoService;
+	
+	@PostMapping(value = "/salvar")
+	public ResponseEntity<ModelLancamentos> salvarLancamento(@RequestBody @Valid ModelLancamentos modelLancamentos, HttpServletResponse response){
+		ModelLancamentos lancamentoSalvo = lancamentoService.salvarLancamento(modelLancamentos, response);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
 
 	@GetMapping(value = "/listar")
 	public List<ModelLancamentos> listar(){
@@ -25,8 +38,9 @@ public class LancamentoController {
 	}
 
 	@GetMapping(value = "/{codigo}")
-	public ModelLancamentos pesquisaPorCodigo(@PathVariable Long codigo) {
-		return lancamentoService.pesquisaPorCodigo(codigo);
+	public ResponseEntity<ModelLancamentos> pesquisaPorCodigo(@PathVariable Long codigo) {
+		ModelLancamentos lancamento = lancamentoService.pesquisaPorCodigo(codigo);
+		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
 	}
 
 }
--- src/main/java/br/com/systemsgs/service/LancamentosService.java
+++ src/main/java/br/com/systemsgs/service/LancamentosService.java
@@ -2,9 +2,16 @@ package br.com.systemsgs.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.com.systemsgs.event.RecursoCriadoEvent;
 import br.com.systemsgs.exception.RecursoNaoEncontradoException;
 import br.com.systemsgs.model.ModelLancamentos;
 import br.com.systemsgs.repository.LancamentoRepository;
@@ -14,6 +21,18 @@ public class LancamentosService {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	@Transactional
+	public ModelLancamentos salvarLancamento(ModelLancamentos modelLancamentos, HttpServletResponse response){
+		ModelLancamentos lancamentoSalvo = lancamentoRepository.save(modelLancamentos);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		
+		return lancamentoSalvo;
+	}
 	
 	public List<ModelLancamentos> listaLancamentos(){
 		return lancamentoRepository.findAll();
