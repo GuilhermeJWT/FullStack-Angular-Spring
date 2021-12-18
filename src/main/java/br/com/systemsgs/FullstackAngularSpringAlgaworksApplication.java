bce43d03cda964eddd1571b03b9ba9cde277f337
GuilhermeJWT <guiromanno@gmail.com>
2021-12-18 17:50:31 -0300

    Muda o banco para Postgres e modifica as Migrations

--- src/main/java/br/com/systemsgs/FullstackAngularSpringAlgaworksApplication.java
+++ src/main/java/br/com/systemsgs/FullstackAngularSpringAlgaworksApplication.java
@@ -3,7 +3,6 @@ package br.com.systemsgs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import br.com.systemsgs.config.SystemsGsPropertyProfile;
 
